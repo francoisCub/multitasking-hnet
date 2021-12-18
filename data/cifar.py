2c66609443fd76a22c6e48bad860fc401e4c3fe6
francoisCub <francois.cubelier@gmx.com>
2021-12-18 20:55:03 +0100

    Add initial data processing

---
+++ data/cifar.py
@@ -0,0 +1,128 @@
+from typing import Iterator, List
+
+from pytorch_lightning import LightningDataModule
+from torch import LongTensor, randperm, stack, unique
+from torch.utils.data import DataLoader, Sampler, SequentialSampler, Subset
+from torchvision.datasets import CIFAR100
+from torchvision.transforms import (Compose, Normalize, RandomCrop,
+                                    RandomHorizontalFlip, ToTensor)
+
+from data.utils import get_sorted_dataset
+
+
+class CifarBatchSampler(Sampler[List[int]]):
+    def __init__(self, sampler: Sampler[int], batch_size: int, drop_last: bool) -> None:
+        if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
+                batch_size <= 0:
+            raise ValueError("batch_size should be a positive integer value, "
+                             "but got batch_size={}".format(batch_size))
+        if not isinstance(drop_last, bool):
+            raise ValueError("drop_last should be a boolean value, but got "
+                             "drop_last={}".format(drop_last))
+        self.sampler = sampler
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        n = len(sampler)
+        if n % batch_size != 0:
+            raise ValueError(
+                "dataset size should be a multiple of the batch size")
+
+        base_idx = randperm(n//batch_size)
+        self.idx = iter([[base_id*batch_size+i for i in range(batch_size)]
+                        for base_id in base_idx])
+
+    def __iter__(self) -> Iterator[List[int]]:
+        yield from self.idx
+
+    def __len__(self) -> int:
+        return len(self.idx)
+
+
+class LightningCifar(LightningDataModule):
+    def __init__(self, data_dir=".data", batch_size=25, num_class_per_task=10, n_classes=100):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.transform = Compose([RandomCrop(32, padding=4, padding_mode='reflect'),
+                                  RandomHorizontalFlip(),
+                                  ToTensor(),
+                                  Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        self.test_transform = Compose([
+            ToTensor(),
+            Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        self.num_class_per_task = num_class_per_task
+        self.n_classes = n_classes
+
+    def prepare_data(self):
+        CIFAR100(root=self.data_dir, train=True,
+                 download=True, transform=ToTensor())
+        CIFAR100(root=self.data_dir, train=False,
+                 download=True, transform=ToTensor())
+
+    def setup(self, stage):
+        if stage == "fit" or stage is None:
+            data_full = CIFAR100(root=self.data_dir, train=True,
+                                 download=False, transform=self.transform)
+            self.data_train, self.data_val = self.split_dataset(
+                data_full)
+            self.data_train = get_sorted_dataset(
+                self.data_train, self.batch_size)
+            self.data_val = get_sorted_dataset(self.data_val, self.batch_size)
+
+        if stage == "test" or stage is None:
+            self.data_test = CIFAR100(
+                root=self.data_dir, train=False, download=False, transform=self.test_transform)
+            self.data_test = get_sorted_dataset(
+                self.data_test, self.batch_size)
+
+    def train_dataloader(self):
+        return DataLoader(self.data_train, batch_sampler=self.batch_sampler(self.data_train), collate_fn=self.get_collate_fn())
+
+    def val_dataloader(self):
+        return DataLoader(self.data_val, batch_sampler=self.batch_sampler(self.data_val), collate_fn=self.get_collate_fn())
+
+    def test_dataloader(self):
+        return DataLoader(self.data_test, batch_sampler=self.batch_sampler(self.data_test), collate_fn=self.get_collate_fn())
+
+    def batch_sampler(self, dataset):
+        return CifarBatchSampler(SequentialSampler(dataset), batch_size=self.batch_size, drop_last=False)
+
+    def get_collate_fn(self):
+        num_class_per_task = self.num_class_per_task
+
+        def collate_fn(batch):
+            x = stack([x for x, _ in batch])
+            y = LongTensor([y % num_class_per_task for _, y in batch])
+            classes = LongTensor([y for _, y in batch])
+            task = LongTensor([y//num_class_per_task for _, y in batch])
+            task = unique(task)
+            if len(task) > 1:
+                raise RuntimeError("There should be one task per batch")
+            return x, y, classes, task
+        return collate_fn
+
+    def split_dataset(self, dataset):
+        sorted_dataset = get_sorted_dataset(dataset, self.batch_size)
+        if len(sorted_dataset) % self.n_classes != 0:
+            raise ValueError()
+        nbr_images_per_class = len(sorted_dataset) // self.n_classes
+        # idx for a given class
+        perm = randperm(len(sorted_dataset)//self.n_classes)
+        if len(perm) % 10 != 0:
+            raise ValueError()
+        idx = perm[:len(perm)//10]
+        n_val = len(perm)//10
+        val_idxs = []
+        train_idxs = []
+        for cls in range(self.n_classes):
+            perm = randperm(nbr_images_per_class) + \
+                nbr_images_per_class*cls  # idx for a given class
+            if len(perm) % 10 != 0:
+                raise ValueError()
+            val_idx = perm[0:n_val].tolist()
+            train_idx = perm[n_val:nbr_images_per_class].tolist()
+            val_idxs.extend(val_idx)
+            train_idxs.extend(train_idx)
+        val_dataset = Subset(sorted_dataset, val_idxs)
+        train_dataset = Subset(sorted_dataset, train_idxs)
+        return train_dataset, val_dataset
---
+++ data/utils.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+from torch import LongTensor, stack, unique
+from torch.utils.data import ConcatDataset, Subset
+from tqdm import tqdm
+
+
+def get_sorted_dataset(dataset, batch_size, log=True):
+    if log:
+        print(f"Initial length: {len(dataset)}")
+    indices = defaultdict(list)
+    for idx, (x, y) in enumerate(tqdm(dataset)):
+        indices[y].append(idx)
+    for k in indices.keys():
+        indices[k] = indices[k][:len(
+            indices[k])-(len(indices[k]) % batch_size)]
+    datasets = [Subset(dataset, idx) for (cls, idx) in indices.items()]
+    if log:
+        print(f"{len(datasets)} classes")
+    sorted_dataset = ConcatDataset(datasets)
+    if log:
+        print(f"Final length {len(sorted_dataset)}")
+
+    return sorted_dataset
+
+
+def get_collate_fn_task(num_class_per_task):
+    def collate_fn(batch):
+        x = stack([x for x, _ in batch])
+        y = LongTensor([y % num_class_per_task for _, y in batch])
+        classes = LongTensor([y for _, y in batch])
+        task = LongTensor([y//num_class_per_task for _, y in batch])
+        task = unique(task)
+        if len(task) > 1:
+            raise RuntimeError("There should be one task per batch")
+        return x, y, classes, task
+    return collate_fn
