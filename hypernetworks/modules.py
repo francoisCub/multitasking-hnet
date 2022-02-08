dee685c7ae891a425a87c66d0c6fb0241c8228cd
francoisCub <francois.cubelier@gmx.com>
2022-02-08 10:44:48 +0100

    Fix scaling factor for initialisation

--- hypernetworks/modules.py
+++ hypernetworks/modules.py
@@ -8,17 +8,15 @@ class Selector(Module):
         self.end = start + size
         self.total_size = size
         if len(shape) == 2:
-            self.scaling_factor = sqrt(6 / (shape[-1] + shape[-2]))
+            self.scaling_factor = sqrt(6 / (3*(shape[-1] + shape[-2])))
         elif len(shape) == 4:
-            self.scaling_factor = sqrt(1 / shape[1]*shape[-1]*shape[-2])
+            self.scaling_factor = sqrt(1 / (3*(shape[1]*shape[-1]*shape[-2])))
         else:
-            self.scaling_factor = sqrt(1 / shape[-1])
+            self.scaling_factor = sqrt(1 / (3*shape[-1]))
         
 
     def forward(self, x):
         return x[:, self.start:self.end] * self.scaling_factor
     
     def extra_repr(self) -> str:
-        return 'total_size={}'.format(
-            self.total_size
-        )
+        return f'total_size={self.total_size}, scaling={self.scaling_factor:.4f}'
