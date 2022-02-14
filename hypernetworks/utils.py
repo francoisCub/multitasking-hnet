380f704f11c836f05c10c22927c5826fb18135fd
francoisCub <francois.cubelier@gmx.com>
2022-02-14 09:47:49 +0100

    Update estimate connectivity

--- hypernetworks/utils.py
+++ hypernetworks/utils.py
@@ -14,15 +14,16 @@ def compute_nbr_params(model):
 
 
 def estimate_connectivity(hnet, latent_size):
+    # when target model is huge and latent size is not small, does not scale
     with no_grad():
         z = nn.functional.one_hot(LongTensor(
             range(latent_size)), latent_size).float().cuda()
         out = hnet(z)
-    res = sum(out != 0, dim=1) / out.shape[1]
-    z = ones(1, latent_size).cuda()
-    out = hnet(z)
-    coverage = sum(out != 0, dim=1) / out.shape[1]
-    return mean(res), min(res), max(res), median(res), res, mean(coverage)
+        res = sum(out != 0, dim=1) / out.shape[1]
+        z = ones(1, latent_size).cuda()
+        out = hnet(z)
+        coverage = sum(out != 0, dim=1) / out.shape[1]
+        return mean(res), min(res), max(res), median(res), res, mean(coverage)
 
 # delete_param and set_param from https://discuss.pytorch.org/t/how-does-one-have-the-parameters-of-a-model-not-be-leafs/70076/10
 
